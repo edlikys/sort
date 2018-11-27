96d606a8f1e56900e905aefda0329524500e32b9
edlikys <43602811+edlikys@users.noreply.github.com>
2018-11-27 14:25:30 +0300

    sort

---
+++ sort.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+import numpy as np
+
+def insertionsort(n):
+    a = np.random.randint(0, 100, n)
+    print(a)
+    for i in range(len(a)):
+        j = i - 1
+        key = a[i]
+        while a[j] > key and j >= 0:
+            a[j+1] = a[j]
+            j -= 1
+        a[j+1] = key
+    return a
+
+new_arr = insertionsort(10)
+print(new_arr)
+
+
+# In[ ]:
+
+
+
+
